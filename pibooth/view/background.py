b8667862fd48d25de5144a7474e826cf0fe818d3
Michael Garrez <michael.garrez@gmail.com>
2025-05-24 11:40:03 +0200

    change size forget

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -568,13 +568,13 @@ class PrintBackground(Background):
 
         text = get_translated_text("print_forget")
         if text:
-            rect = pygame.Rect(self._rect.width // 2, 0,
+            rect = pygame.Rect(self._rect.width // 2 + 30, 0,
                                self._rect.width // 5 - 2 * self._text_border,
-                               self._rect.height * 0.3 - 2 * self._text_border)
+                               self._rect.height * 0.25 - 2 * self._text_border)
             if self.arrow_location == ARROW_TOP:
                 rect.top = self._rect.height * 0.08
             else:
-                rect.bottom = self._rect.height - self._rect.height * 0.08
+                rect.bottom = self._rect.height - 20
 
             self._write_text(text, rect)
 
